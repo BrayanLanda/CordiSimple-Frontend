12620959d56e26c87b22bb9b826f3b78060df9f5
Brayan Acosta <landakkpp@gmail.com>
2024-11-14 23:01:39 -0500

    add register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from './pages/home/home.component';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServeErrorComponent } from './errors/serve-error/serve-error.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'errors', component: TestErrorsComponent},
     {path: 'not-found', component: NotFoundComponent},
     {path: 'server-error', component: ServeErrorComponent},
-    {path: '**', component: HomeComponent, pathMatch: 'full'}
+    {path: '**', component: HomeComponent, pathMatch: 'full'},
+    {path: 'dashboard', component: DashboardComponent}
 ];
--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,12 +1,52 @@
-import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, inject, OnInit, output } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { TextInputComponent } from '../../shared/text-input/text-input.component';
+import { AccountService } from '../../services/account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf, TextInputComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  private accountService = inject(AccountService);
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+  cancelRegister = output<boolean>();
+  registerForm: FormGroup = new FormGroup({});
+  validationErrors: string[] | undefined;
 
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.registerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), 
+          Validators.maxLength(10)]],
+    });
+  }
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value === control.parent?.get(matchTo)?.value ? null : {isMatching: true}
+    }
+  }
+
+  register() {
+    this.accountService.register(this.registerForm.value).subscribe({
+      next: _ => this.router.navigateByUrl('/dashboard') ,
+      error: error => this.validationErrors = error
+    })
+  }
+
+  cancel() {
+    this.cancelRegister.emit(false);
+  }
 }
